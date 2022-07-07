9b7dd693d1ba8ec65b32a516899245b862e32784
geraldo <85137475+Jikjii@users.noreply.github.com>
2022-07-06 21:33:57 -0400

    Layout Implemented

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+export function Header(){
+    return (
+        <div className="flex justify-evenly">
+            <div className="flex-none">Icon</div>
+            <div className="flex-none">Post</div>
+            <div className="flex-none">Create New Post</div>
+        </div>
+    )
+}
---
+++ src/components/Layout.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import Link from "next/link";
+import Head from "next/head";
+import { motion } from "framer-motion";
+
+const Layout = ({}) => {
+    <motion.div
+        exit={{ opacity: 0}}
+        initial="initial"
+        animate="animate"
+    >
+
+    </motion.div>
+};
+
+export default Layout;
